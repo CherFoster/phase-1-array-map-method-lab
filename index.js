396425c57f8c3b4da22b36ac97386b12a6602108
Cherise Foster <cherise.foster@outlook.com>
2023-03-05 14:59:53 -0700

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(line => {
+    let words = line.split(' ')
+    let response = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    return response.join(' ')
+  })
+
 }
